6de8ec46e71336b458e48ccbb509987fd4b2e0b7
nimaid <ellagjameson@gmail.com>
2022-04-24 06:40:37 -0700

    1.2.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('LICENSE') as f:
     
 setup(
     name='dl621',
-    version='0.1.2',
+    version='0.1.2.1',
     description='A simple library to download e621 images with embedded tags',
     long_description=readme,
     author='Ella Jameson',
