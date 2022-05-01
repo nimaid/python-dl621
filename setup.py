568381df7ffc920da65345be41ab760009a67439
nimaid <ellagjameson@gmail.com>
2022-04-30 21:20:01 -0700

    Fixed large file bug

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ with open('LICENSE') as f:
     
 setup(
     name='dl621',
-    version='1.5.0',
+    version='1.5.1',
     description='A simple python module and CLI utility to download e621 images with embedded XMP tags and description',
     long_description=readme,
     author='Ella Jameson',
     author_email='ellagjameson@gmail.com',
     url='https://github.com/nimaid/python-dl621',
     license=license,
-    install_requires=["imgtag>=1.0.6", "requests"],
+    install_requires=["imgtag>=1.0.7", "requests>=2.27.1"],
     packages=find_packages(exclude=('tests', 'docs')),
     entry_points={
         'console_scripts': [
