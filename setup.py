e6cae7bb4ecde73571e3543802b2dac2f127ee96
nimaid <ellagjameson@gmail.com>
2022-05-30 08:49:18 -0700

    forced handling of memory errors

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ with open('LICENSE') as f:
     
 setup(
     name='dl621',
-    version='2.1.6',
+    version='2.1.7',
     description='A simple python module and CLI utility to download e621 images with embedded XMP tags and description',
     long_description=readme,
     author='Ella Jameson',
     author_email='ellagjameson@gmail.com',
     url='https://github.com/nimaid/python-dl621',
     license=license,
-    install_requires=["imgtag>=1.1.5", "requests", "sockets"],
+    install_requires=["imgtag>=1.1.6", "requests", "sockets"],
     packages=find_packages(exclude=('tests', 'docs')),
     entry_points={
         'console_scripts': [
